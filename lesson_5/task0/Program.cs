757efc5ba0dfcd945214b3f4b56c4658f617399e
ZF-1000 <n.shanin@mail.ru>
2022-07-12 20:54:35 +0300

    lesson_5

---
+++ lesson_5/task0/Program.cs
@@ -0,0 +1,35 @@
+﻿/*
+Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+Напишите программу, которая покажет количество чётных чисел в массиве.
+
+[345, 897, 568, 234] -> 2
+*/
+
+using System;
+using static System.Console;
+Clear();
+
+int[] array = GetRandomArray(4, 100, 1000);
+WriteLine($"[{String.Join(",", array)}] -> {GetCntEvenNumb(array)}");
+
+
+int GetCntEvenNumb(int[] array)
+{
+    int cnt = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        cnt += array[i] % 2 == 0 ? 1 : 0;
+    }
+    return cnt;
+}
+
+
+int[] GetRandomArray(int size, int minValue, int maxValue)
+{
+    int[] result = new int[size];
+    for(int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return result;
+}
